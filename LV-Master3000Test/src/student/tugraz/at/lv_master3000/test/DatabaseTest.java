fba4f556c8b10cba59bad8832cac8af6e31e1d73
dopedi <doris.pedratscher@student.tugraz.at>
2013-05-14 22:34:45 +0200

    doesn't work

---
+++ LV-Master3000Test/src/student/tugraz/at/lv_master3000/test/DatabaseTest.java
@@ -0,0 +1,7 @@
+package student.tugraz.at.lv_master3000.test;
+
+import junit.framework.TestCase;
+
+public class DatabaseTest extends TestCase{
+
+}
---
+++ LV-Master3000Test/src/student/tugraz/at/lv_master3000/test/HomeworkTest.java
@@ -0,0 +1,30 @@
+package student.tugraz.at.lv_master3000.test;
+
+import java.util.Date;
+import junit.framework.TestCase;
+import student.tugraz.at.lv_master3000.Homework;
+
+public class HomeworkTest extends TestCase {
+
+	
+	private Homework hw;
+	private Date due;
+	
+	protected void setUp() throws Exception {
+		super.setUp();
+		hw = new Homework();
+		due = new Date(2012, 3, 12);
+		hw.setDueDate(due);
+		
+	}
+
+	protected void tearDown() throws Exception {
+		super.tearDown();
+		hw = null;
+	}
+	
+	public void testDueDate(){
+		assertEquals(due, hw.getDueDate());
+	} 
+
+}
