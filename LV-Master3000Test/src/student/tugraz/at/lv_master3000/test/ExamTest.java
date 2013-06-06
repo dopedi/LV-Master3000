f137fd1c6ef342a08aff21684dfa5027dc472e0a
Amra Dzombic <adzombic@student.tugraz.at>
2013-06-06 12:21:56 +0200

    again and again and again

--- LV-Master3000Test/src/student/tugraz/at/lv_master3000/test/ExamTest.java
+++ LV-Master3000Test/src/student/tugraz/at/lv_master3000/test/ExamTest.java
@@ -2,6 +2,7 @@ package student.tugraz.at.lv_master3000.test;
 
 import android.test.AndroidTestCase;
 import student.tugraz.at.lv_master3000.Book;
+import student.tugraz.at.lv_master3000.Exam;
 import student.tugraz.at.lv_master3000.Lecture;
 
 import java.util.Date;
@@ -24,29 +25,29 @@ public class ExamTest extends AndroidTestCase
 
     public void testGetterAndSetters() throws Exception
     {
-        String bookName = "Book Test";
-        String authorName = "Testo Testic";
-        Date dueDate = new Date(2013, 6, 6);
-        String lenderName = "Lender Test";
-        String lenderAddress = "Address Test";
+        Date examDate = new Date(2013, 6, 6);
+        String examDay = "SomeDay";
+        String examLocation = "Address Test";
+        int lectureId = 1;
+        int examId = 2;
 
         Lecture lecture = new Lecture("Lecture Test");
 
-        Book book = new Book(bookName, lecture);
+        Exam exam = new Exam(lectureId);
 
-        book.setAuthorName(authorName);
-        assertEquals(authorName, book.getAuthorName());
+        assertEquals(lectureId, exam.getLectureId());
 
-        assertEquals(bookName, book.getBookName());
+        exam.setId(examId);
+        assertEquals(examId, exam.getId());
 
-        book.setDueDate(dueDate);
-        assertEquals(dueDate, book.getDueDate());
+        exam.setDate(examDate);
+        assertEquals(examDate, exam.getDate());
 
-        book.setLenderName(lenderName);
-        assertEquals(lenderName, book.getLenderName());
+        exam.setDay(examDay);
+        assertEquals(examDay, exam.getDay());
 
-        book.setLenderAddress(lenderAddress);
-        assertEquals(lenderAddress, book.getLenderAddress());
+        exam.setLocation(examLocation);
+        assertEquals(examLocation, exam.getLocation());
 
     }
 
