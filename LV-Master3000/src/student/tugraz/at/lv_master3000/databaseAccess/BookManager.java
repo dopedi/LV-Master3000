32d1523a09d182f27be35719b5ebaa691e2def52
dosn89 <doris.pedratscher@student.tugraz.at>
2013-06-07 18:06:02 +0200

    manager files

---
+++ LV-Master3000/src/student/tugraz/at/lv_master3000/databaseAccess/BookManager.java
@@ -0,0 +1,63 @@
+package student.tugraz.at.lv_master3000.databaseAccess;
+
+import android.content.ContentValues;
+import android.content.Context;
+import student.tugraz.at.lv_master3000.Book;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: doris
+ * Date: 6/6/13
+ * Time: 6:35 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class BookManager extends LVMaster3000DBHelper{
+    public static  final String tableName = "book";
+    private int rowCount = 0;
+
+    public BookManager(Context context) {
+        super(context);
+    }
+    public Integer insertNewBook(Book book){
+
+        ContentValues values = new ContentValues();
+        values.put("lectureId", book.getLecture());
+
+        java.util.Date date = book.getDueDate();
+        java.sql.Date sqlDate;
+        if(date != null){
+            sqlDate = new java.sql.Date(date.getTime());
+            values.put("due_date", sqlDate.toString());
+        }
+
+        values.put("name", book.getBookName());
+        values.put("lecture", book.getLecture());
+        values.put("author", book.getAuthorName());
+        values.put("lender_name", book.getLenderName());
+        values.put("lender_address", book.getLenderAddress());
+
+        db.insert(tableName, "null", values);
+
+        rowCount++;
+
+        return rowCount-1;
+    }
+
+
+
+    public Book getBookFromDB(int id){
+        Book result = null;
+        // TODO fetch from DB
+
+        return result;
+    }
+
+    public Book getBookFromDBByName(String mobapp) {
+        Book result = null;
+
+        String selectStmt = "select * from " + tableName;
+        //Cursor cursor = db.query();
+
+        return result;
+    }
+}
