1df8cd2cf5798d5a634c9aae03770da340a79b10
Amra Dzombic <adzombic@student.tugraz.at>
2013-06-17 18:20:42 +0200

    activities xml done

---
+++ LV-Master3000/src/student/tugraz/at/lv_master3000/Activities/LectureActivity.java
@@ -0,0 +1,17 @@
+package student.tugraz.at.lv_master3000.Activities;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: dzom-dzom
+ * Date: 6/15/13
+ * Time: 4:57 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class LectureActivity extends BaseActivity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
---
+++ LV-Master3000/src/student/tugraz/at/lv_master3000/Activities/OverviewActivity.java
@@ -0,0 +1,22 @@
+package student.tugraz.at.lv_master3000.Activities;
+
+import android.os.Bundle;
+import android.widget.ListView;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: dzom-dzom
+ * Date: 6/15/13
+ * Time: 4:46 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class OverviewActivity extends BaseActivity
+{
+    private ListView listView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
